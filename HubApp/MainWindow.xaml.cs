d45adb75b364b0484a0f61ba092f707595b5644b
David Brewster <brewcrew5498@outlook.com>
2016-11-24 01:06:33 -0600

    almost done

--- HubApp/MainWindow.xaml.cs
+++ HubApp/MainWindow.xaml.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Threading;
-using System.Drawing;
 using System.Diagnostics;
 using HubApp.Twitter;
 using Tweetinvi.Models;
@@ -27,8 +20,8 @@ namespace HubApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string TRACK = "#MAGA";
-        private const int SLEEP_TIME = 2500;
+        private const string TRACK = "#MannequinChallenge";
+        private const int SLEEP_TIME = 5000;
         private const string DEFAULT_TWITTER_PIC = "https://camo.githubusercontent.com/93c261622c63780cb29c6423b0787c8de460aff6/687474703a2f2f636c6f75642e73636f74742e65652f696d616765732f6e756c6c2e706e67";
         private WindowState _lastWindowState;
         private WindowStyle _lastWindowStyle;
@@ -44,92 +37,129 @@ namespace HubApp
             InitializeWindowOptions();
         }
 
-        public void InitializeTwitterStream()
+        private void InitializeTwitterStream()
         {
             Thread thread = new Thread(TwitterView_StartStreaming);
             thread.Start();
         }
 
-        public void InitializeTwitterQueue()
+        private void InitializeTwitterQueue()
         {
             _tweetQueue = new Queue<ITweet>();
             Thread thread = new Thread(TwitterView_Queue);
             thread.Start();
         }
 
-        public void InitializeTwitterCard()
+        private void InitializeTwitterCard()
         {
             //this.twitterMedia.Source = new BitmapImage(new Uri(DEFAULT_TWITTER_PIC));
         }
 
-        public void InitializeBackgroundVideo()
+        private void InitializeBackgroundVideo()
         {
             this.bgvideo.MediaEnded += new RoutedEventHandler(mediaElement_OnMediaEnded);
             this.bgvideo.LoadedBehavior = MediaState.Manual;
             this.bgvideo.Play();
         }
 
-        public void InitializeWindowOptions()
+        private void InitializeWindowOptions()
         {
             _lastWindowState = this.WindowState;
             _lastWindowStyle = this.WindowStyle;
             this.KeyDown += new KeyEventHandler(MainWindow_KeyDown);
+            this.SizeChanged += new SizeChangedEventHandler(MainWindow_SizeChanged);
         }
 
-        public void TwitterView_StartStreaming()
+        private void TwitterView_StartStreaming()
         {
+
             TwitterView twitter = new TwitterView();
             var tweetStream = Stream.CreateFilteredStream();
+
+            // add filter
             tweetStream.AddTrack(TRACK);
+            // add action event
             tweetStream.MatchingTweetReceived +=
                   new EventHandler<Tweetinvi.Events.MatchedTweetReceivedEventArgs>(TwitterView_ReceivedPost);
             tweetStream.StartStreamMatchingAllConditions();
         }
 
-        public void TwitterView_Queue()
+        private void TwitterView_Queue()
         {
             if (_tweetQueue.Count != 0)
             {
+                // get first tweet out of queue
                 ITweet tweet = _tweetQueue.Dequeue();
                 var link = tweet.Url;
                 var user = tweet.CreatedBy;
                 var text = tweet.FullText;
-                var media = tweet.Media.Count == 0 ? DEFAULT_TWITTER_PIC : tweet.Media[0].MediaURLHttps;
+                var media = tweet.Media.Count == 0 ? DEFAULT_TWITTER_PIC : tweet.Media[0].MediaURL;
                 var userPic = user.ProfileImageUrlHttps;
                 var screenName = user.ScreenName;
 
+
+                // go on Window thread and change the contents of the View 
                 Dispatcher.BeginInvoke(new Action(delegate ()
                 {
                     this.profilePic.Source = new BitmapImage(new Uri(userPic));
                     this.screenName.Text = string.Format("@{0}", screenName);
+                    this.realName.Text = user.ToString();
                     this.tweetText.Text = text;
-                    this.twitterMedia.Source = new BitmapImage(new Uri(media));
+                    
                     this.tweetMediaBg.Background =
                     new BrushConverter().ConvertFromString(
                         string.Format("#{0}", user.ProfileBackgroundColor)
                     ) as SolidColorBrush;
+
+                    
+                    if (tweet.Media[0].MediaType == "video" && tweet.Media[0].VideoDetails.Variants[0].URL.Last().Equals('4'))
+                    {
+                        this.twitterMedia.Visibility = Visibility.Hidden;
+                        this.twitterVideo.Visibility = Visibility.Visible;
+
+                        this.twitterMedia.Source = null;
+
+                        // Convert https to http if need be
+                        this.twitterVideo.Source = new Uri(tweet.Media[0].VideoDetails.Variants[0].URL.Remove(4, 1));
+                    }
+                    else
+                    {
+                        this.twitterVideo.Visibility = Visibility.Hidden;
+                        this.twitterMedia.Visibility = Visibility.Visible;
+
+                        this.twitterVideo.Source = null;
+                        this.twitterMedia.Source = new BitmapImage(new Uri(media));
+                    }
+
+                    
                 }));
             }
             Thread.Sleep(SLEEP_TIME);
             TwitterView_Queue();
         }
 
-        public void TwitterView_ReceivedPost(object sender, Tweetinvi.Events.MatchedTweetReceivedEventArgs args)
+        private void TwitterView_ReceivedPost(object sender, Tweetinvi.Events.MatchedTweetReceivedEventArgs args)
         {
+            // add to the queue only if there is a picture in the tweet
             if (args.Tweet.Media.Count > 0)
             {
                 _tweetQueue.Enqueue(args.Tweet);
+
+                // debugging
+                Trace.WriteLine(string.Format("Tweets in Queue: {0}", _tweetQueue.Count));
             }
         }
 
-        public void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
+            // toggle fullscreen
             if (e.Key == Key.F11)
             {
                 if (WindowStyle == WindowStyle.None)
                 {
                     WindowState = _lastWindowState;
                     WindowStyle = _lastWindowStyle;
+                    ResizeMode = ResizeMode.CanResize;
 
                 }
                 else
@@ -138,12 +168,21 @@ namespace HubApp
                     _lastWindowStyle = WindowStyle;
                     WindowState = WindowState.Maximized;
                     WindowStyle = WindowStyle.None;
+                    ResizeMode = ResizeMode.NoResize;
                 }
             }
         }
 
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Size newSize = e.NewSize;
+            double height = newSize.Height, width = newSize.Width;
+            this.tweetText.FontSize = (height + width) / 125;
+        }
+
         private void mediaElement_OnMediaEnded(object sender, RoutedEventArgs e)
         {
+            // start video over
             this.bgvideo.Position = new TimeSpan(0, 0, 1);
             this.bgvideo.Play();
         }
--- HubApp/Twitter/TwitterView.cs
+++ HubApp/Twitter/TwitterView.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿using System.IO;
 using System.Web.Helpers;
-using System.Reflection;
 using Tweetinvi;
 
 namespace HubApp.Twitter
@@ -13,19 +7,33 @@ namespace HubApp.Twitter
     class TwitterView
     {
         private const string CREDENTIALS_FILE = "Resources/keys.json";
+
+        /// <summary>
+        ///     Instantiates the Twitter class. Potentially will have parameters.
+        /// </summary>
         public TwitterView()
         {
             string userCredentials = CollectUserCredentials();
             AuthenticateUser(userCredentials);
-            TestAuthentication("Hello, World! This message was sent by a C# program.");
         }
 
+        /// <summary>
+        ///     Retreives user credentials
+        /// </summary>
+        /// <returns>
+        ///     string containing the contents of the key json file.
+        /// </returns>
         private string CollectUserCredentials()
         {
             string jsonContents = File.ReadAllText(CREDENTIALS_FILE);
             return jsonContents;
 
         }
+
+        /// <summary>
+        ///     Authenticates the user based on the json string.
+        /// </summary>
+        /// <param name="json"></param>
         private void AuthenticateUser(string json)
         {
             dynamic keys = Json.Decode(json);
@@ -36,6 +44,11 @@ namespace HubApp.Twitter
                 keys.AccessTokenSecret
             );
         }
+
+        /// <summary>
+        ///     Publishes a post to the user's account to verify the user authentication was valid.
+        /// </summary>
+        /// <param name="message"></param>
         private void TestAuthentication(string message)
         {
             Tweet.PublishTweet(message);
